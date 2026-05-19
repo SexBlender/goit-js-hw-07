15ea0164a5c5eb3833dbe9f2b7778568c2b7588b
AntonD <adikhtenko@gmail.com>
2026-05-19 19:30:26 +0300

    hw-7

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+
+const changeBtn = document.querySelector('.change-color');
+const input = document.querySelector('.color');
+
+changeBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  input.textContent = color;
+});
