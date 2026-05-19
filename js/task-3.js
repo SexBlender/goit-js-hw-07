905b25988361aa1e30bf398ad6500d3bb58e393d
AntonD <adikhtenko@gmail.com>
2026-05-19 20:00:27 +0300

    hw-7

--- js/task-3.js
+++ js/task-3.js
@@ -10,10 +10,10 @@ const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
 input.addEventListener('input', () => {
-  const value = input.value;
+  const value = input.value.trim();
   if (value === '') {
     output.textContent = 'Anonymous';
   } else {
-    output.textContent = input.value.trim();
+    output.textContent = value;
   }
 });
